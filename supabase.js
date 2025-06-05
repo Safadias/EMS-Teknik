614e554b30a78233eb9fa633999df94fa58c8c92
Safadias <53385053+Safadias@users.noreply.github.com>
2025-06-05 14:04:22 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
 
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://your-project.supabase.co";
-const SUPABASE_KEY = "public-anon-key";
+const SUPABASE_URL = "https://fasajsconpwfnkjmkoxp.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhc2Fqc2NvbnB3Zm5ram1rb3hwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMjQ0NjksImV4cCI6MjA2NDcwMDQ2OX0.6GbhFuTUliFN4uMF1SwfP_uBiiw8X5p3_0aieFvqNDc";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
